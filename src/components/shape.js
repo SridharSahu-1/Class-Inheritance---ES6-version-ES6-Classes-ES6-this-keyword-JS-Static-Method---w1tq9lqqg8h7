d39c5bb31855e30462f3683714ecd48c0c7bcf42
Sridhar Sahu <74735881+SridharSahu-1@users.noreply.github.com>
2023-01-20 22:27:04 +0530

    Update shape.js

--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,17 @@
 // declare class
 
 // export class using module.exports
+class Shape {
+     constructor() {
+          this.color = "green";
+     }
+     drawShape() {
+
+     }
+     calculateArea() {
+
+     }
+}
+
+// export class using module.exports
+module.exports = Shape;
